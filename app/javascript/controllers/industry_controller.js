c7831257bbfe6b8accc185e8a74fc188935e1c1b
guillaumeingrin <guillaume.ingrin@gmail.com>
2024-03-12 10:23:44 +0100

    glide error

--- app/javascript/controllers/industry_controller.js
+++ app/javascript/controllers/industry_controller.js
@@ -1,28 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+//import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="industry"
-import Glide from "@glidejs/glide";
+//import Glide from "@glidejs/glide";
 
-export default class extends Controller {
-  connect() {
-    this.initializeGlide();
-  }
+//export default class extends Controller {
+//  connect() {
+//    this.initializeGlide();
+//  }
 
-  initializeGlide() {
-    const glide = new Glide(this.element, {
-      type: "carousel",
-      startAt: 0,
-      perView: 3,
+ // initializeGlide() {
+  //  const glide = new Glide(this.element, {
+//      type: "carousel",
+ //     startAt: 0,
+  //    perView: 3,
       // Autres options Glide.js
-    });
+//    });
 
-    glide.mount();
-  }
-  prevSlide() {
-    this.glide.go("<");
-  }
+//    glide.mount();
+//  }
+//  prevSlide() {
+//    this.glide.go("<");
+//  }
 
-  nextSlide() {
-    this.glide.go(">");
-  }
-}
+//  nextSlide() {
+ //   this.glide.go(">");
+ // }
+//}
